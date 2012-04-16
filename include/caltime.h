15c0adf98df5dfb60517cf37f9def244ba881d15
Christian Steiner <christian.steiner@inaos.ch>
2012-04-16 14:19:35 +0200

    updated decl

--- include/caltime.h
+++ include/caltime.h
@@ -11,10 +11,10 @@ struct caltime {
   long offset;
 } ;
 
-extern void caltime_tai();
-extern void caltime_utc();
+extern void caltime_tai(struct caltime *ct, struct tai *t);
+extern void caltime_utc(struct caltime *ct, struct tai *t, int *pwday, int *pyday);
 
-extern unsigned int caltime_fmt();
-extern unsigned int caltime_scan();
+extern unsigned int caltime_fmt(char *s, struct caltime *ct);
+extern unsigned int caltime_scan(char *s, struct caltime *ct);
 
 #endif
